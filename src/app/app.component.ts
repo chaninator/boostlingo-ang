70369870426ca4f7d6feacb553a1f89951b7cba9
Andrew Chan <chaninator@Andrews-MBP.attlocal.net>
2024-07-17 15:48:31 -0500

    boostlingo SPA assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { LocalStorageService } from './local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HomeComponent,
+    LoginComponent,
+    NavComponent,
+    RouterModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Boostlingo-SPA';
+  isLoggedIn: string | null;
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {
+    this.isLoggedIn = this.localStorageService.getItem('loggedIn');
+  }
+
+  renderView() {
+    if (this.isLoggedIn === 'true') {
+      console.log('logged in');
+      this.router.navigate(['']);
+    } else {
+      console.log('not logged in');
+      this.router.navigate(['login']);
+    }
+  }
+
+  ngOnInit() {
+    this.renderView();
+  }
 }
